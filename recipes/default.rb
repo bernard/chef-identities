f660922b7c5435578eaf52add5ad78b31666aa68
Jean-Francois Theroux <jtheroux@lapresse.ca>
2013-02-04 16:46:24 -0500

    added ruby-devel dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,15 @@
 package 'rubygems' do
 end
 
+case node[:platform_family]
+  when 'rhel', 'suse'
+    package 'ruby-devel' do
+    end
+  when 'debian'
+    package 'ruby-dev' do
+    end
+end
+
 execute 'install_ruby_shadow' do
   command 'gem install ruby-shadow'
 end
