630eeabd556ed30b53a63599feaf94b183ce5e71
Jean-Francois Theroux <jtheroux@lapresse.ca>
2013-02-06 12:52:19 -0500

    now using chef_gem to install ruby-shadow

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,22 +6,5 @@
 #
 # License: GPLv2
 
-package 'rubygems' do
-end
-
-case node[:platform_family]
-  when 'rhel', 'suse'
-    [ 'make', 'gcc', 'ruby-devel' ].each do |pkg|
-      package pkg do
-      end
-    end
-  when 'debian'
-    [ 'make', 'gcc', 'ruby-dev' ].each do |pkg|
-      package pkg do
-      end
-    end
-end
-
-execute 'install_ruby_shadow' do
-  command 'gem install ruby-shadow'
+chef_gem 'ruby-shadow' do
 end
