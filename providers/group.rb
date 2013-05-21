7d85f20a1b1bb84bcc5ece24648f4228feee42d1
Jean-Francois Theroux <failshell@gmail.com>
2013-05-21 14:26:38 -0400

    forgot to remove the chef-solo condition check for groups

--- providers/group.rb
+++ providers/group.rb
@@ -1,35 +1,31 @@
-if Chef::Config[:solo]
-  Chef::Log.warn("This recipe does not support Chef Solo. It requires search and data bags.")
-else
-  action :manage do
-    members_list = Array.new
-    search(new_resource.data_bag, "groups:#{new_resource.name}") do |m|
-      members_list << m['id']
-    end
+action :manage do
+  members_list = Array.new
+  search(new_resource.data_bag, "groups:#{new_resource.name}") do |m|
+    members_list << m['id']
+  end
 
-    unless new_resource.members.nil?
-      new_resource.members.each do |e|
-        members_list << e
-      end
-    end
-
-    group new_resource.name do
-      begin
-        Etc.getgrnam(new_resource.name)
-        action :manage
-      rescue ArgumentError
-        action :create
-      end
-      gid new_resource.gid if new_resource.gid
-      members members_list if members_list
-      append new_resource.append if new_resource.append
-      system new_resource.system if new_resource.system
+  unless new_resource.members.nil?
+    new_resource.members.each do |e|
+      members_list << e
     end
   end
 
-  action :remove do
-    group new_resource.name do
-      action :remove
+  group new_resource.name do
+    begin
+      Etc.getgrnam(new_resource.name)
+      action :manage
+    rescue ArgumentError
+      action :create
     end
+    gid new_resource.gid if new_resource.gid
+    members members_list if members_list
+    append new_resource.append if new_resource.append
+    system new_resource.system if new_resource.system
+  end
+end
+
+action :remove do
+  group new_resource.name do
+    action :remove
   end
 end
