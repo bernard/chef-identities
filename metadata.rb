8e76560d1ee997a7f20d921958b217be8ee1d696
Jean-Francois Theroux <me@failshell.io>
2014-08-06 21:15:16 -0400

    updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,13 @@ maintainer_email 'me@failshell.io'
 license          'Apache 2.0'
 description      'Installs/Configures identities'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.3'
+version          '0.3.4'
+
+%w[
+  ubuntu
+  centos
+  redhat
+  suse
+].each do |os|
+  supports os
+end
