1b1ed70804b7607780114285186ae067dac2fa46
Jean-Francois Theroux <me@failshell.io>
2014-10-28 14:19:21 -0400

    updated group lwrp

--- resources/group.rb
+++ resources/group.rb
@@ -1,12 +1,7 @@
 actions :manage, :remove
+default_action :manage
 
-attribute :data_bag, :kind_of => String, :default => "users"
+attribute :append, :default => false
+attribute :data_bag, :kind_of => String, :default => 'users'
 attribute :gid
 attribute :members, :kind_of => Array
-attribute :system, :default => false
-attribute :append, :default => false
-
-def initialize(*args)
-  super
-  @action = :manage
-end
