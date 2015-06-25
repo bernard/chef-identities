863b4a6008e2ecdc2676e17c8311ed29b0d2d8f4
Jean-Francois Theroux <me@failshell.io>
2015-06-24 22:50:46 -0400

    starting with a clean slate

--- resources/user.rb
+++
@@ -1,9 +0,0 @@
-actions :manage, :remove, :lock, :unlock, :cleanup
-default_action :manage
-
-attribute :data_bag, :kind_of => String, :default => 'users'
-attribute :encrypted_databag, :default => false
-attribute :home_dir_perms, :kind_of => Integer, :default => 0700
-attribute :secret_file, :default => '/etc/chef/encrypted_data_bag_secret'
-attribute :shell, :kind_of => String
-attribute :vault_data_bag, :kind_of => String, :default => 'users_vault'
