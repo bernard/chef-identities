6539bbb2ae93b60e427149d04c8b799203f9de98
Jean-Francois Theroux <me@failshell.io>
2015-06-24 22:55:07 -0400

    berks cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'identities'
+maintainer       'Jean-Francois Theroux'
+maintainer_email 'me@failshell.io'
+license          'MIT'
+description      'Installs/Configures identities'
+long_description 'Installs/Configures identities'
+version          '0.1.0'
