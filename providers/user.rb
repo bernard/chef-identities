f0911270bf1544f584a4ac5e54b675a88d2d4651
Jean-Francois Theroux <me@failshell.io>
2015-06-26 11:07:47 -0400

    bugfix

--- providers/user.rb
+++ providers/user.rb
@@ -43,6 +43,7 @@ action :manage do
       end
 
       template "#{home_dir}/.ssh/authorized_keys" do
+        cookbook 'identities'
         owner new_resource.name
         group new_resource.name
         variables(:keys => new_resource.authorized_keys)
